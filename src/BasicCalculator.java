981faf8682c8f868132ac1f36823171205c48cf9
rainbowpython <39071822+rainbowpython@users.noreply.github.com>
2022-10-10 00:18:30 -0700

    coments coments lots of coments

--- src/BasicCalculator.java
+++ src/BasicCalculator.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
-/* main notes
-Due to long dinner, and not so good planning, I was unable to code this to its fullest
-I would have liked to have implemented
 
- */
 public class BasicCalculator {
     public BasicCalculator(){
         boolean running = true;
 
         while (running == true) {
+            //instantiates Scanner object
             Scanner scanner = new Scanner(System.in);
 
+            //asks and recieves input
             System.out.println("What is the first value?");
             double firstNumber = scanner.nextDouble();
 
-
+            //asks and recieves second input
             System.out.println("next number?");
             double secondNumber = scanner.nextDouble();
 
+            //
             System.out.println("what operator?\n1: +  2: *  3: -  4: / 5: %");
             String operator = scanner.next();
 
+            //computes using the 2 values given
             switch (operator) {
                 case "+", "1" -> System.out.println(firstNumber + secondNumber);
                 case "*", "2" -> System.out.println(firstNumber * secondNumber);
@@ -30,23 +30,24 @@ public class BasicCalculator {
                 default -> throw new RuntimeException("Error: not a recognizable operation");
             }
             
-            System.out.println("what would you like to do next?");
-            System.out.println("1: restart  2: choose new calculator  3: exit");
-
+            System.out.println("what would you like to do next\n1: restart  2: choose new calculator  3: exit");
 
             //made a for loop to meet a requirement
+            //if one is entered, will go back to the start of the basicCalculator
             if (scanner.nextInt() == 1) {
 
             } else if (scanner.nextInt() == 2) {
                 Calculators.chooseCalculator();
             }else {
-                running = false;
+                running = aBoolean(false);
             }
 
         }
 
-
     }
+
+    //Returns false if given false, true if given true
+    //I don't expect extra credit points, but this was created in the name of hvaing them
     public boolean aBoolean(Boolean boo){
         if (boo ==true){return true;}
         else if(boo==false) {return false;}
--- src/Calculators.java
+++ src/Calculators.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
-
+//please refer to README.md
+//will NOT work with java 8
 public class Calculators {
     public static void main(String[] args){
         chooseCalculator();
-
     }
+
+
     public static void chooseCalculator(){
         System.out.println("What calculator would you like to use?");
-        Scanner scanner = new Scanner(System.in);
         System.out.println("1: basic calculator  2: side of a triangle ");
+        Scanner scanner = new Scanner(System.in);
+
+
         switch (scanner.nextInt()){
-            case 1:
-                BasicCalculator basicCalculator = new BasicCalculator();
-                break;
-            case 2:
-                TriangleCalculator triangleCalculator = new TriangleCalculator();
+            case 1 ->BasicCalculator basicCalculator = new BasicCalculator();
+            case 2 ->TriangleCalculator triangleCalculator = new TriangleCalculator();
         }
 
     }
