3562de14abf053315df512d566302750a943a175
rainbowpython <joshble@outlook.com>
2022-10-09 22:19:16 -0700

    easd
    eas

--- src/TriangleCalculator.java
+++ src/TriangleCalculator.java
@@ -7,7 +7,8 @@ public class TriangleCalculator {
         while (running){
             Scanner scanner = new Scanner(System.in);
 
-            System.out.println("What side of a triangle would you like to calculate");
+            System.out.println("What side of a triangle would you like to calculate\n1: A  2: B  3: C");
+
         }
 
     }
