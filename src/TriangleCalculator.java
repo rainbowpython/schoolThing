d53bf44b6c4ee941b7e3789cdb2a6659c34f2e0e
rainbowpython <joshble@outlook.com>
2022-10-09 22:06:26 -0700

    tri1
    dd

--- src/TriangleCalculator.java
+++ src/TriangleCalculator.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class TriangleCalculator {
     public TriangleCalculator(){
+        boolean running = true;
+
+        while (running){
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("What side of a triangle would you like to calculate");
+        }
 
     }
 }
